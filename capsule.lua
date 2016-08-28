122d358d19df68d0da76387023467f049cd5a700
Paul <paul@niobium.me>
2016-08-28 13:35:48 +0200

    stuff

---
+++ capsule.lua
@@ -0,0 +1,16 @@
+local args = {...}
+local tempName = "/.capsule-temp"
+
+if(args[1] == "capsule-install-internal") then
+  fs.makeDir("/usr/bin/capsule.deps")
+  shell.run("/usr/bin/glue init /usr/bin/capsule.deps")
+  local handle = fs.open("/usr/bin/capsule.deps/GlueFile","w")
+  handle.write([[
+  depend "Progdor" version "2" method "ignore"
+  depend "json" namespace "JSON" method "dofile"
+  ]])
+elseif(args[1] == "capsule-uninstall-internal") then
+  fs.delete("/usr/bin/capsule.deps")
+else
+
+end
