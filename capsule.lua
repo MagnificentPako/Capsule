f8c4a3e5308d701f33ffa3d2f3a8ba4f2ca276a3
Paul <paul@niobium.me>
2016-08-28 13:40:52 +0200

    derp

--- capsule.lua
+++ capsule.lua
@@ -1,7 +1,7 @@
 local args = {...}
 local tempName = "/.capsule-temp"
 
-if(args[1] == "capsule-install-internal") then
+if(args[1] == "install-capsule-internal") then
   fs.makeDir("/usr/bin/capsule.deps")
   shell.run("/usr/bin/glue init /usr/bin/capsule.deps")
   local handle = fs.open("/usr/bin/capsule.deps/GlueFile","w")
@@ -9,7 +9,7 @@ if(args[1] == "capsule-install-internal") then
   depend "Progdor" version "2" method "ignore"
   depend "json" namespace "JSON" method "dofile"
   ]])
-elseif(args[1] == "capsule-uninstall-internal") then
+elseif(args[1] == "uninstall-capsule-internal") then
   fs.delete("/usr/bin/capsule.deps")
 else
 
